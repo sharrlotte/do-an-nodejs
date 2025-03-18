97bbff9bb392286e2dd80b8ccac9e788dea09fc2
Dat-upoverloading <trandinhdat003@gmail.com>
2025-03-18 09:12:00 +0700

    Cập nhật giao diện: thêm component Novel để hiển thị danh sách tiểu thuyết, cập nhật trang chính để bao gồm component này, và cải thiện các thành phần UI như button và carousel.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from './Header';
 import Footer from './Footer';
+import Novel from './novel';
 export default function Home() {
   return (
     <main className="overflow-y-auto overflow-x-hidden h-full bg-gray-200">
       <div className="p-4 min-h-dvh">
         <Header />
+        <Novel />
         <Footer />
       </div>
     </main>
