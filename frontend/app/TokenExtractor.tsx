8e26709aaf7108630d701fe66ce6df82dd7bfaaf
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-04-07 18:11:46 +0700

    refactor(frontend): replace localStorage with useLocalStorage hook in TokenExtractor
    The localStorage usage was replaced with the useLocalStorage hook to manage token storage more effectively. This change improves code maintainability and ensures consistent token handling. Additionally, the refresh logic was moved to a separate useEffect to trigger only when the token is available.

--- frontend/app/TokenExtractor.tsx
+++ frontend/app/TokenExtractor.tsx
@@ -4,9 +4,15 @@ import { useSession } from '@/context/SessionContext';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { useLocalStorage } from 'usehooks-ts';
 
 export default function TokenExtractor() {
+  const [token, setToken] = useLocalStorage('token', '', {
+    deserializer: (value) => value,
+  });
+
   const params = useSearchParams();
+
   const router = useRouter();
 
   const { refresh } = useSession();
@@ -15,13 +21,18 @@ export default function TokenExtractor() {
     const token = params.get('token');
 
     if (token) {
-      localStorage.setItem('token', token);
+      setToken(token);
       const n = new URLSearchParams(params);
       n.delete('token');
       router.push(`?${n.toString()}`);
-      setTimeout(() => refresh(), 1000);
     }
-  }, [params, router, refresh]);
+  }, [params, router, setToken]);
+
+  useEffect(() => {
+    if (token) {
+      refresh();
+    }
+  }, [refresh, token]);
 
   return <></>;
 }
